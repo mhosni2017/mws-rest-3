d5784da6ca184d95b2ada4ba73ccc07af9480886
Mohamed <Mhosni@yahoo.com>
2018-10-26 18:55:32 -0400

    first

---
+++ app/js/dbhelper.js
@@ -0,0 +1,196 @@
+/**
+ * Common database helper functions.
+ */
+
+/*self.importScripts('idb.js');*/
+
+class DBHelper {
+
+  /**
+   * API URL.
+   * Change this to restaurants.json file location on your server.
+   */
+  static get API_URL() {
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}`;
+  }
+
+  /**
+   * Fetch all restaurants.
+   */
+  static fetchRestaurants(callback, id) {
+    //let xhr = new XMLHttpRequest();
+    let fetchURL;
+    if (!id) {
+      fetchURL = `${DBHelper.API_URL}/restaurants`;
+      }  else {
+        fetchURL=`${DBHelper.API_URL}/restaurants`+'/'+id;
+      }
+      fetch(fetchURL, {method: 'GET'}).then(response=> {
+          console.log(fetchURL);
+          console.log(response);
+          response.json().then(restaurants=> {
+              console.log('Restaurant JSON:', restaurants);
+              callback(null,restaurants);
+          }).catch(error=> {
+            callback(`Request failed. Returned ${error}`, null);
+          });
+      }).catch(error=> {
+        callback(`Request failed. Returned ${error}`, null);
+      });
+
+  }
+
+  /**
+   * Fetch a restaurant by its ID.
+   */
+  static fetchRestaurantById(id, callback) {
+    // fetch all restaurants with proper error handling.
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const restaurant = restaurants.find(r => r.id == id);
+        if (restaurant) { // Got the restaurant
+          callback(null, restaurant);
+        } else { // Restaurant does not exist in the database
+          callback('Restaurant does not exist', null);
+        }
+      }
+    });
+  }
+
+  static fetchReviewsByRestaurantId(restaurant_id) {
+      return fetch(`${DBHelper.API_URL}/reviews/?restaurant_id=${restaurant_id}`).then(response => {
+        if (!response.ok) return Promise.reject("Reviews couldn't be fetched from network");
+        return response.json();
+      }).then(fetchedReviews => {
+        // if reviews could be fetched from network:
+        // TODO: store reviews on idb
+        return fetchedReviews;
+      }).catch(networkError => {
+        // if reviews couldn't be fetched from network:
+        // TODO: try to get reviews from idb
+        console.log(`${networkError}`);
+        return null; // return null to handle error, as though there are no reviews.
+      });
+    }
+
+  /**
+   * Fetch restaurants by a cuisine type with proper error handling.
+   */
+  static fetchRestaurantByCuisine(cuisine, callback) {
+    // Fetch all restaurants  with proper error handling
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Filter restaurants to have only given cuisine type
+        const results = restaurants.filter(r => r.cuisine_type == cuisine);
+        callback(null, results);
+      }
+    });
+  }
+
+  /**
+   * Fetch restaurants by a neighborhood with proper error handling.
+   */
+  static fetchRestaurantByNeighborhood(neighborhood, callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Filter restaurants to have only given neighborhood
+        const results = restaurants.filter(r => r.neighborhood == neighborhood);
+        callback(null, results);
+      }
+    });
+  }
+
+  /**
+   * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
+   */
+  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        let results = restaurants
+        if (cuisine != 'all') { // filter by cuisine
+          results = results.filter(r => r.cuisine_type == cuisine);
+        }
+        if (neighborhood != 'all') { // filter by neighborhood
+          results = results.filter(r => r.neighborhood == neighborhood);
+        }
+        callback(null, results);
+      }
+    });
+  }
+
+  /**
+   * Fetch all neighborhoods with proper error handling.
+   */
+  static fetchNeighborhoods(callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Get all neighborhoods from all restaurants
+        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+        // Remove duplicates from neighborhoods
+        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+        callback(null, uniqueNeighborhoods);
+      }
+    });
+  }
+
+  /**
+   * Fetch all cuisines with proper error handling.
+   */
+  static fetchCuisines(callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Get all cuisines from all restaurants
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+        // Remove duplicates from cuisines
+        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+        callback(null, uniqueCuisines);
+      }
+    });
+  }
+
+  /**
+   * Restaurant page URL.
+   */
+  static urlForRestaurant(restaurant) {
+    return (`./restaurant.html?id=${restaurant.id}`);
+  }
+
+  /**
+   * Restaurant image URL.
+   */
+  static imageUrlForRestaurant(restaurant) {
+    return (`/img/${restaurant.photograph}`);
+  }
+
+  /**
+   * Map marker for a restaurant.
+   */
+  static mapMarkerForRestaurant(restaurant, map) {
+    const marker = new google.maps.Marker({
+      position: restaurant.latlng,
+      title: restaurant.name,
+      url: DBHelper.urlForRestaurant(restaurant),
+      map: map,
+      animation: google.maps.Animation.DROP}
+    );
+    return marker;
+  }
+
+}
---
+++ app/sw.js
@@ -0,0 +1,108 @@
+/*import idb from "idb";*/
+self.importScripts('idb.js');
+/* code used here from the udacity course and google developers Caching Files with Service Worker samples*/
+const dbPromise = idb.open("rest-db",1, upgradeDB => {
+  switch (upgradeDB.oldVersion) {
+    case 0: upgradeDB.createObjectStore("restaurants" , {keyPath :'id'});
+  }
+});
+let staticCacheName = 'mws-restaurant-cache-1';
+var allCaches = [
+  staticCacheName
+];
+let urlsToCache = [
+  '/index.html',
+  '/restaurant.html',
+  'css/styles.css',
+  'js/main.js',
+  'js/dbhelper.js',
+  'js/restaurant_info.js'
+];
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log("inside install")
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("inside activate")
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Requests going to the API get handled separately from those // going to other destirations twist
+self.addEventListener('fetch', function(event) {
+  let cacheRequest = event.request;
+  let requestUrl = new URL(event.request.url);
+  if (event.request.url.indexOf("restaurant.html") >-1 ) {
+    const cacheURL = "restaurant.html";
+    cacheRequest = new Request(cacheURL);
+  }
+  if (requestUrl.port ==='1337') {
+   const parts = requestUrl.pathname.split("/");
+   const id = parts[parts.length - 1] === "restaurants" ? "-1" : parts[parts.length - 1];
+      handleAJAXEvent(event, id);
+    } else { handleNonAJAXEvent(event,cacheRequest);
+    }
+ }) ;
+
+const handleAJAXEvent = (event, id) => { // Check the IndexedDB to see if the JSON for the API
+event.respondWith( dbPromise.then(db => {
+    return db.transaction("restaurants").objectStore("restaurants").get(id);
+    }).then(data => {
+    return ( (data && data.data) || fetch(event.request).then(fetchResponse => fetchResponse.json()).then(
+      json => {
+        return dbPromise.then(db => {
+          const tx = db.transaction("restaurants","readwrite");
+          tx.objectStore("restaurants").put({
+            id: id,
+            data: json
+          });
+          return json; }); })
+        ); }).then( finalResponse=> {
+             return new Response(JSON.stringify(finalResponse));
+          }).catch(error => { return new Response("Frror fetching data", { status: 500});
+        })
+      ); };
+
+const handleNonAJAXEvent = (event,cacheRequest) => {
+
+  event.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return caches.match(cacheRequest).then(function(response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch( error=> {
+        if (event.request.url.indexOf(".jpg">=1)) {
+          return caches.match("/img/na.png");
+        }
+        return new Response (
+          "Application is not connected to internet" ,
+           {
+             status: 404,
+             statusText : "Application is not connected to internet"
+           }
+
+        );
+        })
+
+    })
+  );
+};
